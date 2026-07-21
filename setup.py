2942da71d74c0f8c50a5095d4fe7162458dff66b
Mohammad Iskandarany <m7md.k.isk@gmail.com>
2026-07-21 15:06:03 +0300

    minor bump

--- setup.py
+++ setup.py
@@ -4,17 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.launchpad",
-    version="0.9.1",
+    version="0.10.0",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
-    url=(
-        "https://github.com/canonical-web-and-design/"
-        "canonicalwebteam.launchpad"
-    ),
-    description=(
-        "Trigger builds of snaps and ubuntu images"
-        "through the launchpad API."
-    ),
+    url=("https://github.com/canonical-web-and-design/canonicalwebteam.launchpad"),
+    description=("Trigger builds of snaps and ubuntu imagesthrough the launchpad API."),
     packages=find_packages(),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
